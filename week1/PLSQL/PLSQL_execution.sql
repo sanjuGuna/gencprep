2f9e483ab19712156651576263b97f1ca87a77b5
sanjuGuna <sanjay27it@gmail.com>
2026-06-24 19:10:33 +0530

    plsql control structures execution

---
+++ week1/PLSQL/PLSQL_execution.sql
@@ -0,0 +1,47 @@
+set serveroutput on;
+
+declare
+    a number := 15;
+begin
+    if a < 18 then
+        dbms_output.put_line('You are not eligible');
+    else
+        dbms_output.put_line('You are eligible');
+    end if;
+end;
+/
+set serveroutput on;
+
+declare
+    a char(1) := 'A';
+begin
+    case a
+        when 'A' then
+            dbms_output.put_line('grade is A');
+        when 'B' then
+            dbms_output.put_line('grade is B');
+        when 'C' then
+            dbms_output.put_line('grade is C');
+        when 'D' then
+            dbms_output.put_line('grade is D');
+        else
+            dbms_output.put_line('Fail');
+    end case;
+end;
+/
+set serveroutput on;
+
+declare
+    mark number := 50;
+begin
+    if mark >= 90 then
+        dbms_output.put_line('Grade A');
+    elsif mark >= 75 then
+        dbms_output.put_line('grade B');
+    elsif mark >= 50 then
+        dbms_output.put_line('grade C');
+    else
+        dbms_output.put_line('Fail');
+    end if;
+end;
+/
