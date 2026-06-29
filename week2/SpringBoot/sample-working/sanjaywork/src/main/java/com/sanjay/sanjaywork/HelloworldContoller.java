b4ea8a110c08cfca88fd76fd596b52362bb7a56a
sanjuGuna <sanjay27it@gmail.com>
2026-06-29 23:42:05 +0530

    TDD learning

--- week2/SpringBoot/sample-working/sanjaywork/src/main/java/com/sanjay/sanjaywork/HelloworldContoller.java
+++ week2/SpringBoot/sample-working/sanjaywork/src/main/java/com/sanjay/sanjaywork/HelloworldContoller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldContoller {
-    @GetMapping("/hello")
+    @GetMapping("/")
     String sayHelloWorld(){
         return "<h1>Hello World!!</h1>";
     }
