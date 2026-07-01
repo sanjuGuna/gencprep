df69593131db041dab2445b5667c0dfd19f07e27
sanjuGuna <sanjay27it@gmail.com>
2026-07-01 19:04:33 +0530

    verifying the interaction between methods

--- week1/Testing/demo/src/test/java/com/samplejunit/demo/EmployeeServiceTest.java
+++ week1/Testing/demo/src/test/java/com/samplejunit/demo/EmployeeServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,14 +29,15 @@ public class EmployeeServiceTest {
     void testGetEmployeeById() {
         //Arrange
         Employee employee = new Employee(1,"sanjay",15000);
-        //Stubbing
+        //Stubbing -> tells the mock what to return.
         when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
         //Act
         Employee emp = employeeService.findById(1);
         //Assert
         assertEquals(1,emp.getId());
         assertEquals("sanjay",emp.getName());
-
+        //verify interaction -> verifies if the method was actually called.
+        verify(employeeRepository).findById(1);//checks whether the findById method was actually called in repository.
 
     }
 }
