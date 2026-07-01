7cb88c875bef35e8dc269ba5e3964b4cf2b16090
sanjuGuna <sanjay27it@gmail.com>
2026-07-01 14:58:06 +0530

    Text fixtures practice

--- week1/Testing/demo/src/test/java/com/samplejunit/demo/CalculatorTest.java
+++ week1/Testing/demo/src/test/java/com/samplejunit/demo/CalculatorTest.java
@@ -1,16 +1,35 @@
 package com.samplejunit.demo;
 
 import com.samplejunit.demo.service.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*; //use static imports to improve readability and reduce boilerplate
 //without static, we need to use its class name as prefix to use its methods eg. Assertion.assertEquals()
 
 public class CalculatorTest {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll");
+    }
+    Calculator calculator;
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("beforeEach");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach");
+    }
     @Test
     void additionTest(){
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
         int result = calculator.add(1,2);
         //System.out.println(result);
+        System.out.println("Addition test");
         assertEquals(3,result);
         assertNotEquals(0,result);
         assertTrue(result > 0);
@@ -20,12 +39,12 @@ public class CalculatorTest {
     @Test
     void subtractionTest(){
         //Arrange
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
 
         //Act
         int result = calculator.subtract(2,2);
         //System.out.println(result);
-
+        System.out.println("Subtraction test");
         //Assert
         assertEquals(0,result);
         assertNotEquals(1,result);
@@ -34,7 +53,8 @@ public class CalculatorTest {
     }
     @Test
     void divideTest(){
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
+        System.out.println("Divide test");
         assertThrows(ArithmeticException.class,()->{calculator.divide(2,0);});
     }
 }
